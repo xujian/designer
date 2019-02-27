c5423fcec0ed81b7b930b4ecd1b1ca416d4b1739
xujian <782135@qq.com>
2019-02-27 10:55:05 +0800

    vue add quasar

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import './styles/quasar.styl'
+import 'quasar-extras/material-icons'
+import 'quasar-extras/fontawesome'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+})
+
 Vue.config.productionTip = false
 
 new Vue({
