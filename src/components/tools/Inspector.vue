22b744f861aed4fdb9929afcb7f8001e74f4e188
xujian <782135@qq.com>
2019-05-06 23:57:54 +0800

    trivial style update

--- src/components/tools/Inspector.vue
+++ src/components/tools/Inspector.vue
@@ -5,10 +5,7 @@
     </header>
     <main>
       <pa-chartlib-inspector
-        v-model="value.controlProps"
-        @change="onChange"></pa-chartlib-inspector>
-      <pa-chartlib-inspector
-        v-model="value.chartProps"
+        v-model="mergedProps"
         @change="onChange"></pa-chartlib-inspector>
     </main>
   </div>
@@ -19,6 +16,11 @@ import { Inspector as PaChartlibInspector } from 'vue-chartlib/support'
 export default {
   name: 'PaInspector',
   props: ['value'],
+  computed: {
+    mergedProps () {
+      return [...this.value.chartProps, ...this.value.controlProps]
+    }
+  },
   methods: {
     onChange (props) {
       this.$bus.emit('canvas', {
@@ -43,6 +45,10 @@ export default {
 
 <style lang="stylus">
 .control-inspector
+  header
+    padding 0 12px
+    background-color rgba(0,150,138,0.17)
   h6
-    font-size 14px
+    font-size 12px
+    font-weight 100
 </style>
